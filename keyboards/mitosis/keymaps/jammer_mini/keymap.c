ccb8136e6c68988836662d283f66f81242bca77a
jon mereria <makgyber@gmail.com>
2020-02-07 22:29:03 +0800

    update jammer_dv, new mini keymap

--- keyboards/mitosis/keymaps/jammer_mini/keymap.c
+++ keyboards/mitosis/keymaps/jammer_mini/keymap.c
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_STD] = LAYOUT( /* Standard; as compatible with dvorak and qwerty as possible */
     KC_Q,        KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,
     KC_A,        KC_S,    KC_D,    KC_F,    KC_G,         KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN,
-    KC_Z,       KC_X,    KC_C,    KC_V,    KC_B,         KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_ENT,
-        KC_ESC, KC_MINS, KC_QUOT, KC_LSFT,                       KC_RSFT,  KC_SLSH, KC_UP,   KC_BSPC,
-        KC_LCTL, KC_LALT,  GUI_T(KC_SPC),  LT(_NUM, KC_ENT),      LT(_FN, KC_SPC), KC_LEFT, KC_DOWN, KC_RGHT
+    KC_Z, KC_X,    KC_C,    KC_V,    KC_B,         KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_ENT,
+                 KC_ESC, KC_MINS, KC_QUOT, KC_LSFT,      KC_RSFT,  KC_SLSH, KC_UP,   KC_BSPC,
+              KC_LCTL, KC_LALT,  GUI_T(KC_SPC),  LT(_NUM, KC_ENT),      LT(_FN, KC_SPC), KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_NUM] = LAYOUT( /* Number layout along the home row for maximum speed*/
