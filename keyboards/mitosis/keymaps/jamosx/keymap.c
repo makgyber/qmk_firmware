b875b7c131a3e7e3af0f38e263fd9bffa3118d2e
Jon Mereria <makgyber@gmail.com>
2021-04-14 21:23:34 +0800

    optimize key layout

--- keyboards/mitosis/keymaps/jamosx/keymap.c
+++ keyboards/mitosis/keymaps/jamosx/keymap.c
@@ -26,27 +26,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_STD] = LAYOUT( /* Standard; as compatible with dvorak and qwerty as possible */
     KC_Q,        KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,
     KC_A,        KC_S,    KC_D,    KC_F,    KC_G,         KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN,
-    KC_Z,        KC_X,    KC_C,    KC_V,    KC_B,         KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_ENT,
-              _______, _______,_______, MO(_NUM),          MO(_NUM), _______, _______, _______,
-              _______, KC_LCTL,LT(_FN, KC_SPC),GUI_T(KC_ENT),    SFT_T(KC_SPC),LT(_FN, KC_BSPC),  KC_RALT, _______
+    LSFT_T(KC_Z), KC_X,  KC_C,    KC_V,    KC_B,    KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_ENT,
+              _______, _______,_______, MO(_NUM),         MO(_NUM), _______, _______, _______,
+              KC_LSFT, KC_LCTL,LT(_FN, KC_SPC),GUI_T(KC_ENT),    LSFT_T(KC_SPC),LT(_FN, KC_BSPC),  KC_RALT, KC_ENT
   ),
  
   [_NUM] = LAYOUT( /* Number layout along the home row for maximum speed*/
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                 KC_F1,   KC_F2,   KC_F3,  KC_F4, KC_BSPC,
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                 KC_F1,   KC_F2,   KC_F3,  KC_F4, KC_SLSH,
     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                 KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_QUOT,
-    KC_LSFT, C(KC_LEFT), C(KC_DOWN), C(KC_RIGHT), KC_UP,     KC_F9,   KC_F10, KC_F11,  KC_F12,   KC_ENT,
+    KC_LSFT, C(KC_LEFT), C(KC_DOWN), C(KC_RIGHT), KC_UP,     KC_F9,   KC_F10, KC_F11,  KC_F12,   _______,
             _______, _______,_______, _______,       _______, _______, _______, _______,
             _______, _______, _______, _______,       _______, _______, _______, _______
   ),
 
   [_FN] = LAYOUT( /* Function Layer, primary alternative layer featuring numpad on right hand,
                                      cursor keys on left hand, and all symbols*/
-    KC_ESC,  _______, KC_UP, _______, KC_GRV,       KC_BSLS, KC_WH_L, KC_MS_UP, KC_BSPC, KC_DEL,
+    KC_ESC,  _______, KC_UP, _______, KC_GRV,       KC_BSLS, KC_WH_L, KC_MS_UP, KC_DEL, KC_SLSH,
     KC_TAB,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_MINS,       KC_EQL, KC_MS_L, KC_MS_D, KC_MS_R, KC_QUOT,
-    KC_LSFT, C(KC_LEFT), C(KC_DOWN), C(KC_RIGHT), KC_LBRC,       KC_RBRC, KC_BTN1, KC_BTN2, KC_9,  KC_SLSH,
+    KC_LSFT, C(KC_LEFT), C(KC_DOWN), C(KC_RIGHT), KC_LBRC,       KC_RBRC, KC_BTN1, KC_BTN2, KC_9, _______,
              _______, _______, _______, _______,       _______, _______, _______, _______,
              _______, _______, _______, _______,       _______, _______, _______, _______
-  ),
+  ), 
+
 
   /* blank key layout template
    * .--------------------------------------------..--------------------------------------------.
