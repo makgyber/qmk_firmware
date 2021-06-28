57865b2d8ed9a3f633bdb4afa1cb6f9e3911b11d
Jon Mereria <makgyber@gmail.com>
2021-06-28 21:59:50 +0800

    finalized pinout

--- keyboards/tabinge/config.h
+++ keyboards/tabinge/config.h
@@ -41,8 +41,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D7, E6, B4, B5 }
-#define MATRIX_COL_PINS {  F5, F6, D4,  C6,     B6, B2, B3, B1, F7 }
+// #define MATRIX_ROW_PINS { D7, E6, B4, B5 }
+// #define MATRIX_COL_PINS {  F5, F6, D4,  C6,   F7 ,  B1, B3,  B2, B6}
+
+#define MATRIX_ROW_PINS { B1, B3, B2,  B6 }
+#define MATRIX_COL_PINS {  D4,D0  , F6,  F7,   B5,  B4, E6, D7,  C6   }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
--- keyboards/tabinge/keymaps/default/keymap.c
+++ keyboards/tabinge/keymaps/default/keymap.c
@@ -17,7 +17,9 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE
+    _BASE, 
+    _NUM,
+    _NAV
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -29,24 +31,24 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,           KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,
+        KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                           KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,  KC_S,  KC_D,  KC_F,  KC_G,           KC_H,      KC_J,    KC_K,  KC_L,    KC_SCLN,
         KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,           KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_ENT,
-                                    KC_SPC,         KC_1,         KC_2,  KC_3
+                                    LT(_NAV, KC_SPC),         LT(_NUM, KC_SPC),    LT(_NAV, KC_BSPC),  KC_TAB
     )
     ,
     [_NUM] = LAYOUT(
-        KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,           KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,  KC_S,  KC_D,  KC_F,  KC_G,           KC_H,      KC_J,    KC_K,  KC_L,    KC_SCLN,
-        KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,           KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_ENT,
-                                    KC_SPC,         KC_1,         KC_2,  KC_3
+        KC_ESC, _______, _______, _______, _______,        KC_BSLS,    KC_7,    KC_8,    KC_9,    KC_0,
+        KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,  _______,        KC_MINS,    KC_4,    KC_5,    KC_6,    KC_QUOT,
+        _______, _______, _______, _______, _______,        KC_EQL,     KC_1,    KC_2,    KC_3,   KC_SLSH,
+                                            _______,        _______, _______, _______
     )
     ,
     [_NAV] = LAYOUT(
-        KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,           KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,  KC_S,  KC_D,  KC_F,  KC_G,           KC_H,      KC_J,    KC_K,  KC_L,    KC_SCLN,
-        KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,           KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_ENT,
-                                    KC_SPC,         KC_1,         KC_2,  KC_3
+        _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,        KC_TILD,    KC_HOME,    KC_UP,    KC_END,  KC_GRV,
+        KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,  G(KC_S),        KC_LPRN,    KC_LEFT,    KC_DOWN,    KC_RIGHT,    KC_RPRN,
+        G(KC_Z), G(KC_X), G(KC_C), G(KC_V), KC_BTN1,         KC_LBRC,  C(KC_LEFT), C(KC_DOWN), C(KC_RIGHT),   KC_RBRC,
+                                            _______,        _______, _______, _______
     )
 };
 
